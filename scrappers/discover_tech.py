64a902cb82fad875489d97c8e1982acfbfa1c8ea
EssEnemiGz <biscenp@gmail.com>
2025-08-12 22:26:05 -0400

    Reworked the source code to make it more legible. Waiting for structurer function.

--- scrappers/discover_tech.py
+++ scrappers/discover_tech.py
@@ -20,10 +20,28 @@ logging.basicConfig(filename='development.log', level=logging.INFO, format="%(as
 results = {}
 
 class Analyzer:
-    def html_body_parser(self, html_labels: List[str], html_body: str):
+    def __init__(self, tech: str) -> None:
+        self.tech = tech
+
+    def html_body_parser(self, html_body: str, html_labels: List[str]) -> List:
         html_key = "|".join(html_labels)
         in_html = re.search(rf"({html_key})", html_body) # Critical in resources, cause the html may be large
-        return in_html
+        if in_html:
+            return [self.tech, True]
+        
+        return [self.tech, False]
+
+    def cookies_analizer(self, cookies: List[str], searched_cookies: List[str]) -> List:
+        confidence = any(cookie in cookies for cookie in searched_cookies)
+        return [self.tech, confidence]
+
+    def headers_analizer(self, headers: Dict[str, str], searched_headers: Dict[str, str]) -> List:
+        for header_value in headers.values():
+            for searched_value in searched_headers.values():
+                if re.search(header_value, searched_value):
+                    return [self.tech, True]
+
+        return [self.tech, False]
 
 class Networking:
     def __init__(self, semaphore) -> None:
@@ -41,97 +59,70 @@ class Networking:
             return []
         return [] 
 
-    async def get_html_body(self, url: str, main_session) -> str:
-        logging.info(f"Starting get_html_body() in domain {url}")
+    async def make_request(self, url: str, main_session) -> Dict:
+        logging.info(f"Starting make_request() in domain {url}")
         try:
             async with self.semaphore:
                 async with main_session.get(url) as response:
                     if response.status != 200:
-                        logging.error(f"get_html_body() failed with {response.status} code")
-                        return ""
+                        logging.error(f"make_request() failed with {response.status} code")
+                        return {}
                     
-                    html_body = await response.text()
-                    return html_body
+                    result: Dict = {
+                        "status": response.status,
+                        "headers": dict(response.headers),
+                        "cookies": {k: v.value for k, v in response.cookies.items()},
+                        "body": await response.text()
+                    }
+                    return result
         except Exception as e:
-            logging.error(f"Failed get_html_body() with exception {e}")
-            return ""
-
-async def main(url: str) -> bool:
-    start_time = time.time()
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status != 200:
-                return False
-
-            for key, value in sources_json.items():
-                results.setdefault(key, {"confidence":0} )
-                actual_results = results[key] 
-                # Check the headers
-                headers = value.get("headers")
-                actual_results["headers"] = False
-                header_values = " ".join(response.headers.values()).lower()
-                confidence = False
-                for info in headers.values():
-                    if re.search(info, header_values):
-                        confidence = True
-                        break
-                
-                if confidence:
-                    actual_results["headers"] = True
-
-                # Check the cookies
-                cookies = value.get("cookies")
-                if len(response.cookies) > 0:
-                    actual_results["cookies"] = False
-                    cookie_values = list(response.cookies.values())
-                    confidence = any(cookie in cookie_values for cookie in cookies)
-                    
-                    if confidence:
-                        actual_results["cookies"] = True
-
-                else:
-                    actual_results["cookies"] = True
-
-                # Confidence
-                confidence = value.get("confidence_weights")
-                if actual_results.get("html"):
-                    actual_results["confidence"] += confidence.get("html")
-
-                if actual_results.get("headers"):
-                    actual_results["confidence"] += confidence.get("headers")
-
-                if actual_results.get("cookies"):
-                    actual_results["confidence"] += confidence.get("cookies")
-
-    end_time = time.time()
-    print("Execution time: ", f"{end_time-start_time:.2f} seconds")
-    return True
+            logging.error(f"make_request() failed with exception: {e}")
+            return {}
 
 async def summoner():
     url = "https://blog.mozilla.org/en"
     semaphore = asyncio.Semaphore(20)
     network = Networking(semaphore)
-    checker = Analyzer()
     route_tasks = []
+    routes_result = []
     html_body_results = []
+    headers_results = []
+    cookies_results = []
 
+    start_time = time.time()
     async with aiohttp.ClientSession() as session:
-        html_body = await network.get_html_body(url, session)
+        response = await network.make_request(url, session)
         for tech, info in sources_json.items():
+            checker = Analyzer(tech)
             paths = info.get("routes", [])
             html_labels = info.get("html", [])
+            headers = info.get("headers", [])
+            cookies = info.get("cookies", [])
             
-            analized_html = checker.html_body_parser(html_labels, html_body)
-            if analized_html:
-                html_body_results.append([True, tech])
+            # Check the HTML
+            analized_html = checker.html_body_parser(response.get("body", ""), html_labels)
+            html_body_results.append(analized_html)
 
+            # Check the headers
+            analized_headers = checker.headers_analizer(response.get("headers", ""), headers)
+            headers_results.append(analized_headers)
+
+            # Check the cookies
+            analized_cookies = checker.cookies_analizer(response.get("cookies", ""), cookies)
+            cookies_results.append(analized_cookies)
+
+            # Check the paths
             for path in paths:
                 route_tasks.append(network.fetch(url+path, session, tech))
 
         routes_result = await asyncio.gather(*route_tasks)
-        print(routes_result)
-        print(html_body_results)
-    print(json.dumps(results, indent=4))
+    end_time = time.time()
+    
+    print(json.dumps(routes_result, indent=4))
+    print(json.dumps(html_body_results, indent=4))
+    print("Headers:", json.dumps(headers_results, indent=4))
+    print(json.dumps(cookies_results, indent=4))
+    print(f"Completed in: {end_time-start_time:.2f}s")
 
 if __name__ == "__main__": 
     asyncio.run(summoner())
