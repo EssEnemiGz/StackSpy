203086e13ad27f104926638003977c513a6e07f9
EssEnemiGz <biscenp@gmail.com>
2025-09-17 15:46:13 -0400

    Changed the structure to OOP to ensure the scalability of the stack_spy

--- stack_spy/discover_tech.py
+++ stack_spy/discover_tech.py
@@ -8,55 +8,62 @@ import aiohttp
 import json
 import time
 
-logging.basicConfig(filename='development.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S") 
-configurator = Configurator()
-sources_json = configurator.open_sources()
 
-async def summoner(url: str):
-    semaphore = asyncio.Semaphore(20)
-    network = Networking(semaphore)
+class Spy:
+    def __init__(self, semaphore: int = 20, sources_route: str = "sources.json") -> None:
+        logging.basicConfig(filename='development.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S") 
 
-    results = {}
-    route_tasks = []
-    results.setdefault(url, {})
+        self.semaphore: int = semaphore
+        self.finished_time: float = 0.0
+        self.output = []
 
-    async with aiohttp.ClientSession() as session:
-        response = await network.make_request(url, session)
-        for tech, info in sources_json.items():
-            results[url].setdefault(tech, {})
-            actual_structure = results[url][tech]
+        self.configurator = Configurator()
+        self.sources_json = self.configurator.open_sources(sources_route)
 
-            checker = Analyzer(tech)
-            paths = info.get("routes", [])
-            html_labels = info.get("html", [])
-            headers_patterns = info.get("headers_patterns", [])
-            cookies_patterns = info.get("cookies_patterns", [])
+    async def worker(self, url: str):
+        semaphore = asyncio.Semaphore(self.semaphore)
+        network = Networking(semaphore)
 
-            # Check the HTML
-            analized_html = checker.html_body_parser(response.get("body", ""), html_labels)
-            actual_structure["html"] = analized_html[1]
+        results = {}
+        route_tasks = []
+        results.setdefault(url, {})
 
-            # Check the headers
-            analized_headers = checker.headers_analizer(response.get("headers", ""), headers_patterns)
-            actual_structure["headers"] = analized_headers[1]
+        async with aiohttp.ClientSession() as session:
+            response = await network.make_request(url, session)
+            for tech, info in self.sources_json.items():
+                results[url].setdefault(tech, {})
+                actual_structure = results[url][tech]
 
-            # Check the cookies
-            analized_cookies = checker.cookies_analizer(response.get("cookies", ""), cookies_patterns)
-            actual_structure["cookies"] = analized_cookies[1]
+                checker = Analyzer(tech)
+                paths = info.get("routes", [])
+                html_labels = info.get("html", [])
+                headers_patterns = info.get("headers_patterns", [])
+                cookies_patterns = info.get("cookies_patterns", [])
 
-            # Check the paths
-            for path in paths:
-                route_tasks.append(network.fetch(url+path, session, tech))
+                # Check the HTML
+                analized_html = checker.html_body_parser(response.get("body", ""), html_labels)
+                actual_structure["html"] = analized_html[1]
 
-        routes_result = await asyncio.gather(*route_tasks)
-        for individual_path in routes_result:
-            results[url][individual_path[0]]["routes"] = individual_path[1]
+                # Check the headers
+                analized_headers = checker.headers_analizer(response.get("headers", ""), headers_patterns)
+                actual_structure["headers"] = analized_headers[1]
 
-    return results
+                # Check the cookies
+                analized_cookies = checker.cookies_analizer(response.get("cookies", ""), cookies_patterns)
+                actual_structure["cookies"] = analized_cookies[1]
 
-if __name__ == "__main__":
-    async def main():
-        queue = await configurator.get_routes(summoner)
+                # Check the paths
+                for path in paths:
+                    route_tasks.append(network.fetch(url+path, session, tech))
+
+            routes_result = await asyncio.gather(*route_tasks)
+            for individual_path in routes_result:
+                results[url][individual_path[0]]["routes"] = individual_path[1]
+
+        return results
+    
+    async def worker_wrapper(self):
+        queue = await self.configurator.get_routes(self.worker)
 
         start_time = time.time()
         executed = await asyncio.gather(*queue, return_exceptions=True)
@@ -64,7 +71,21 @@ if __name__ == "__main__":
         
         cleaner = Cleaner()
         cleaned_execute = cleaner.clean_result(executed)
-        print(json.dumps(cleaned_execute, indent=4))
-        print(f"Completed in: {end_time-start_time:.2f}s")
+        self.output = json.dumps(cleaned_execute, indent=4)
+        self.finished_time = float(f"{end_time-start_time:.2f}")
 
-    asyncio.run(main())
+    def run(self) -> None:
+        asyncio.run(self.worker_wrapper())
+
+    def finished(self) -> float:
+        return self.finished_time
+
+    def get_results(self):
+        return self.output
+
+if __name__ == "__main__":
+    spy = Spy()
+
+    spy.run()
+    print(spy.finished(), "\n")
+    print(spy.get_results())
